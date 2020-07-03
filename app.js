6c37650a3200802df3c90fc9199726d8119c16b7
Chris Postma <cjrpostma@gmail.com>
2020-07-03 17:33:31 -0400

    Add db.js and create db pool connection.

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-app.listen(3000);
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// routes
+
+app.listen(5000, () => {
+  console.log('Listening on port 5000');
+});
