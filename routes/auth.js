5857ea60d62ab1971fb8cef498d208e95cd02cb0
Chris Postma <cjrpostma@gmail.com>
2020-07-06 11:07:13 -0400

    Add validation for /auth/login route.

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,7 @@ const jwtGenerator = require('../utils/jwtGenerator');
 const pool = require('../db');
 const authorization = require('../middleware/authorization');
 
-// POST => /auth/register
-// user_name, user_email, user_password
-
+// POST => /api/v0/auth/register
 router.post(
   '/register',
   [
@@ -16,17 +14,17 @@ router.post(
     check('password').isLength({ min: 6 }),
   ],
   async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(422)
+        .json({ message: 'Invalid inputs passed, please check your data.' });
+    }
+
+    const { email, name, password } = req.body;
+
     try {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty()) {
-        return res
-          .status(422)
-          .json({ message: 'Invalid inputs passed, please check your data.' });
-      }
-
-      const { email, name, password } = req.body;
-
       // check if user exists already
       const user = await pool.query(
         'SELECT * FROM users WHERE user_email = $1',
@@ -57,37 +55,54 @@ router.post(
   }
 );
 
-// POST => /auth/login
-router.post('/login', validators, async (req, res) => {
-  try {
+// POST => /api/v0/auth/login
+router.post(
+  '/login',
+  [check('email').isEmail().normalizeEmail()],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(422)
+        .json({ message: 'Invalid inputs passed, please check your data.' });
+    }
+
     const { email, password } = req.body;
 
-    // check if user exists
-    const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [
-      email,
-    ]);
+    try {
+      // check if user exists
+      const user = await pool.query(
+        'SELECT * FROM users WHERE user_email = $1',
+        [email]
+      );
 
-    if (user.rows.length === 0) {
-      return res.status(401).json({ message: 'Email or password is invalid.' });
+      if (user.rows.length === 0) {
+        return res
+          .status(401)
+          .json({ message: 'Email or password is invalid.' });
+      }
+      // check if incoming password matches password in db
+      const isValidPassword = await bcrypt.compare(
+        password,
+        user.rows[0].user_password
+      );
+
+      if (!isValidPassword) {
+        return res
+          .status(401)
+          .json({ message: 'Email or password is invalid.' });
+      }
+
+      // return jwt token
+      const token = jwtGenerator(user.rows[0].user_id);
+      res.json({ token });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error.');
     }
-    // check if incoming password matches password in db
-    const isValidPassword = await bcrypt.compare(
-      password,
-      user.rows[0].user_password
-    );
-
-    if (!isValidPassword) {
-      return res.status(401).json({ message: 'Email or password is invalid.' });
-    }
-
-    // return jwt token
-    const token = jwtGenerator(user.rows[0].user_id);
-    res.json({ token });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send('Server error.');
   }
-});
+);
 
 // GET => /auth/private
 router.get('/private', authorization, async (req, res) => {
