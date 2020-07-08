d8b296457d9633fe048fb56c93735b653517c913
Chris Postma <cjrpostma@gmail.com>
2020-07-08 17:06:14 -0400

    Add TravisCI, AppVeyor, CodeClimate, Coveralls config. Update README with badges.

--- test/index.js
+++ test/index.js
@@ -1,15 +1,13 @@
 import { expect, server, BASE_URL } from './setup';
 
-describe('Index page test', () => {
+describe('Index router test', () => {
   it('gets base url', (done) => {
     server
       .get(`${BASE_URL}/`)
       .expect(200)
       .end((err, res) => {
         expect(res.status).to.equal(200);
-        expect(res.body.message).to.equal(
-          'Environment variable is coming across.'
-        );
+        expect(res.body.message).to.equal('Hello World!');
         done();
       });
   });
--- test/setup.js
+++ test/setup.js
@@ -6,4 +6,4 @@ import app from '../src/app';
 chai.use(sinonChai);
 export const { expect } = chai;
 export const server = supertest.agent(app);
-export const BASE_URL = '/v1';
+export const BASE_URL = '/api/v1';
